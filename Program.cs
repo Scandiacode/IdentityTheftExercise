c6f96f18778c531e3c9bb61c5cbb35733ced9a11
Logan Harris <logan.harris41@yahoo.com>
2021-09-27 13:36:13 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Day1Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, welcome to the Identity Stealer!");
+            
+            Console.WriteLine("Can you give me your name?");
+            string name = Console.ReadLine();
+            
+            Console.WriteLine("What is your age?");
+            int age = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Can you also give me your hometown.");
+            string town = Console.ReadLine();
+
+            //Console.WriteLine("Your name is " + name + ". Your age is " + age + ". Your hometown is " + town + ".");
+
+            Console.WriteLine($"Your name is {name}. Your age is {age}. Your hometown is {town}");
+        }
+    }
+}
